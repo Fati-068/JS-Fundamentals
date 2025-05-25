ed98c686b78a8704d54c412c81ab0ff58d2dc609
Fatima-azzahra <fatiwinx@yahoo.fr>
2025-05-26 00:16:07 +0100

    task 1 of the ALX FE: 3 lines of the 3 languages

---
+++ 1-multi_languages.js
@@ -0,0 +1,4 @@
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
+// This code prints three different strings to the console, each representing a programming language and a brief description of it.
