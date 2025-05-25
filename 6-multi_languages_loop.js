e9dd058c5a1a26acab2fbcd769fd63e9c099a97f
Fatima-azzahra <fatiwinx@yahoo.fr>
2025-05-26 00:49:36 +0100

    correct task 6 of the ALX FE Week 3

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,3 +1,12 @@
 // 5-array.js
-const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-console.log(langs.join('\n'));
+
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+for (const line of lines) {
+  console.log(line);
+}
+// This code iterates over an array of strings and prints each string to the console, demonstrating a simple loop in JavaScript.
