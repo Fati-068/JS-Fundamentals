87d07f6f11add46bc7a90c75333ba71256cf3bb3
Fatima-azzahra <fatiwinx@yahoo.fr>
2025-05-26 00:20:20 +0100

    task 2 of the ALX FE: write a script that prints a message depending on the arguments passed

---
+++ 2-arguments.js
@@ -0,0 +1,4 @@
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(`${arg1} is ${arg2}`);
+// This code retrieves the first two command line arguments passed to the script and prints them in a formatted string.
